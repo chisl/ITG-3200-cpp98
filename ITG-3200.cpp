df8e6739e1af01247766f1e5a84ce0ddbc7453e5
chisl <info@chisl.io>
2017-12-27 18:14:06 +0100

    Initial commit

---
+++ ITG-3200.cpp
@@ -0,0 +1,13 @@
+/*
+ * name:        ITG-3200
+ * description: Single-chip, digital-output, 3-axis MEMS gyro
+ * manuf:       InvenSense Inc.
+ * version:     0.1
+ * url:         https://www.invensense.com/products/motion-tracking/3-axis/itg-3200/
+ * date:        2016-08-01
+ * author       https://chisl.io/
+ * file:        ITG-3200.cpp
+ */
+
+#include "ITG-3200.hpp"
+
